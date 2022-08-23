2cb17d561a4ccf90589b2b794b6973fda5817760
Atsuhiro Muroyama <atuhiro.duda@gmail.com>
2022-08-24 00:16:15 +0900

    Initial Commit

---
+++ MenterBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenterBook
+//
+//  Created by Atsuhiro Muroyama on 2022/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
